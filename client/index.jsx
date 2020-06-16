63251897e8bc9bc53324f4f5c54b6d5a81a32439
Aruseli <llingveri@gmail.com>
2020-06-16 15:32:49 +0300

    Update index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,3 @@
-import '../imports/startup/client/monitor.js'
-import { getCookie } from "../imports/startup/helpers";
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
@@ -21,23 +18,6 @@ onPageLoad(async sink => {
 
 Meteor.startup(()=>{
     if (Meteor.isProduction) {
-        document.addEventListener('yacounter56224693inited', (e) => {
-            const uuid = getCookie('__opix_uid');
-            console.log('in yandex', uuid);
-            const yaCounter = window['yaCounter56224693'];
-            if (yaCounter) {
-                if (uuid) {
-                    /*ym(56224693, 'setUserID', uuid); //не работает с пакетом react-yandexmetrika, пришлось делать через устаревшее API яндекс метрики
-                    ym(56224693, 'userParams', {UserID: uuid});*/
-                    yaCounter.setUserID(uuid);
-                    yaCounter.userParams({UserID: uuid});
-                }
-                /*ym(56224693, 'getClientID', function(clientID) { //не работает с пакетом react-yandexmetrika
-                    if (window.opix) window.opix('event', 'addClientID', {value: clientID});
-                });*/
-                const clientID = yaCounter.getClientID();
-                if (window.opix) window.opix('event', 'addClientID', {value: clientID});
-            }
-        });
+        
     }
 });
